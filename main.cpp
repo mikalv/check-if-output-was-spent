6e6c36c1761c79f31d7f2b009d5054b6d299f7a3
moneroexamples <moneroexamples@tuta.io>
2016-03-06 16:34:04 +0800

    Code cleaning

--- main.cpp
+++ main.cpp
@@ -182,26 +182,30 @@ int main(int ac, const char* av[]) {
 
     // having our transaction, first we check which output in that
     // transactions are ours. For this we need to go through all outputs
-    // in a transaction.
+    // in a transaction, including miner reward in the given block in case
+    // we found the block containing this transaction.
 
     std::vector<size_t> outputs_ids;
 
     uint64_t money_transfered {0};
     uint64_t miner_money_transfered {0};
 
-
+    // look for our outputs in the transaction and the corresponding block reward
     cryptonote::lookup_acc_outs(account_keys, tx, outputs_ids, money_transfered);
     cryptonote::lookup_acc_outs(account_keys, blk.miner_tx, outputs_ids, miner_money_transfered);
-
 
     // get tx public key from extras field
     crypto::public_key pub_tx_key = cryptonote::get_tx_pub_key_from_extra(tx);
 
-
     if (outputs_ids.size())
     {
-        cout << "We found our outputs: " << endl;
+        print("We found our outputs: \n");
 
+        // iterate over each output found
+        // and create its key_image.
+        //
+        // to check if the given output is spend, we just need to check
+        // whether the correspoding key_image is present in the blockchain
         for (size_t ouput_i: outputs_ids)
         {
             // get tx output
@@ -211,49 +215,50 @@ int main(int ac, const char* av[]) {
             const cryptonote::txout_to_key& tx_out_to_key
                 = boost::get<cryptonote::txout_to_key>(tx_output.target);
 
-            print("Our output: {:s}, amount {:0.4f}\n",
+            print("Our output: {:s}, amount {:0.6f}\n",
                   tx_out_to_key.key, tx_output.amount / 1e12);
 
-            // public transaction key is combined with our private view key
+            // public tx key is combined with our private view key
             // to create, so called, derived key.
             crypto::key_derivation derivation;
 
             if (!generate_key_derivation(pub_tx_key, private_view_key, derivation))
             {
-                cerr << "Cant get derived key for: " << "\n"
-                     << "pub_tx_key: " << private_view_key << " and "
-                     << "private_view_key" << private_view_key << endl;
+                cerr << "Cant get derived key for output with: " << "\n"
+                     << "pub_tx_key: " << private_view_key << endl;
 
                 return 1;
             }
 
-
-            print("Derived key: {:s}\n", derivation);
-
+            //print("Derived key: {:s}\n", derivation);
 
             // generate key_image of this output
             crypto::key_image key_image;
 
-            if (!xmreg::generate_key_image(derivation, ouput_i,
+            if (!xmreg::generate_key_image(derivation,
+                                           ouput_i, /* positoin in the tx */
                                            private_spend_key,
                                            account_keys.m_account_address.m_spend_public_key,
                                            key_image))
             {
-                cerr << "Cant generate key image for tx: "
-                     << cryptonote::get_transaction_hash(tx) << endl;
-
+                cerr << "Cant generate key image for output: "  << pub_tx_key << endl;
                 return 1;
             }
 
 
-            print("Key_image generated: {:s}\n", key_image);
+            print("Key image generated: {:s}\n", key_image);
 
+            // finally check if the key_image generated is present in the blockchain
             bool is_spent = core_storage.have_tx_keyimg_as_spent(key_image);
 
             print("Is output spent?: {}\n", is_spent);
 
-            cout <<  "\n" << endl;
+            cout << endl;
         }
+    }
+    else
+    {
+        print("No our outputs were found in this transaction\n");
     }
 
     cout << "\nEnd of program." << endl;
